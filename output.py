59358811b67291536aa5c0c7b991bf32e2c5cdfa
Banoth Mohan <161817334+Mohansonu@users.noreply.github.com>
2026-08-18 13:08:26 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,46 @@
+===MINI ATM===
+Enter your pin: 1234
+1.Check balance: 
+2.Deposit Balance: 
+3.Withdraw balance: 
+4.Balance Details: 
+EXIT
+Enter your choice: 1
+Your Balance is:  5000
+1.Check balance: 
+2.Deposit Balance: 
+3.Withdraw balance: 
+4.Balance Details: 
+EXIT
+Enter your choice: 2
+Enter your amount to depost: 2000
+Money Succesfully Deposited
+New Balance 7000
+1.Check balance: 
+2.Deposit Balance: 
+3.Withdraw balance: 
+4.Balance Details: 
+EXIT
+Enter your choice: 3
+Enter your amount: 3000
+Collect your cash
+New balance:  4000
+1.Check balance: 
+2.Deposit Balance: 
+3.Withdraw balance: 
+4.Balance Details: 
+EXIT
+Enter your choice: 4
+
+Account Holder name:  Mohan Banoth
+
+Account Balance:  4000
+
+Account Type:  savings account
+1.Check balance: 
+2.Deposit Balance: 
+3.Withdraw balance: 
+4.Balance Details: 
+EXIT
+Enter your choice: 5
+Thank you
